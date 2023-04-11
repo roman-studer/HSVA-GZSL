d34faf3299ad80e78eda9b3f9ef8cab184018057
Roman Studer <studer.r@outlook.com>
2023-04-11 13:52:16 +0200

    added POLLEN dataset to data_loader

--- data_loader.py
+++ data_loader.py
@@ -10,15 +10,23 @@ from pathlib import Path
 import pickle
 import copy
 
+from sklearn.preprocessing import MinMaxScaler
+
+
 def map_label(label, classes):
     mapped_label = torch.LongTensor(label.size())
     for i in range(classes.size(0)):
-        mapped_label[label==classes[i]] = i
+        mapped_label[label == classes[i]] = i
 
     return mapped_label
 
+
+
+
+
 class DATA_LOADER(object):
-    def __init__(self, dataset, aux_datasource, device='cuda'):
+    def __init__(self, dataset, aux_datasource, device='cuda', config=None):
+        self.config = config
 
         print("The current working directory is")
         print(os.getcwd())
@@ -30,7 +38,7 @@ class DATA_LOADER(object):
 
         print('Project Directory:')
         print(project_directory)
-        data_path = '/home/shimingchen/ZSL/HSVA/data'
+        data_path = str(project_directory) + '/data'
         print('Data Path')
         print(data_path)
         sys.path.append(data_path)
@@ -50,40 +58,102 @@ class DATA_LOADER(object):
             self.datadir = self.data_path + '/AWA1/'
         elif self.dataset == 'AWA2':
             self.datadir = self.data_path + '/AWA2/'
-        elif self.dataset == 'APY':
-            self.datadir = self.data_path + '/APY/'
-        elif self.dataset == 'FLO':
-            self.datadir = self.data_path + '/FLO/'
 
+        if self.dataset == 'POLLEN':
+            self.read_pollendata()
+        else:
+            self.read_matdataset()
 
-        self.read_matdataset()
         self.index_in_epoch = 0
         self.epochs_completed = 0
 
+    def __len__(self):
+        return self.ntrain
+
+    def get_pollen_data(self):
+        # load pollen data from pickle file
+        if self.config['subset']:
+            path = self.data_path + '/' + self.config['dataset'] + '/pollen_data_subset.pkl'
+        else:
+            path = self.data_path + '/' + self.config['dataset'] + '/pollen_data.pkl'
+        if self.config['split'] is not None:
+            path = path.replace('pollen_data', str(self.config['split']) + '_pollen_data')
+
+        with open(path, 'rb') as f:
+            data = pickle.load(f)
+        return data
 
     def next_batch(self, batch_size):
         idx = torch.randperm(self.ntrain)[0:batch_size]
         batch_feature = self.data['train_seen']['resnet_features'][idx]
-        batch_label =  self.data['train_seen']['labels'][idx]
+        batch_label = self.data['train_seen']['labels'][idx]
         batch_att = self.aux_data[batch_label]
-        return [ batch_feature, batch_att, batch_label]
-        
-    def next_unseen_batch(self, batch_size):
-        idx_unseen = torch.randperm(self.ntest_unseen)[0:batch_size]
-        # batch_unseen_feature = self.data['test_unseen']['resnet_features'][idx_unseen]
-        batch_unseen_label =  self.data['test_unseen']['labels'][idx_unseen]
-        batch_unseen_att = self.aux_data[batch_unseen_label]
-        return [batch_unseen_att, batch_unseen_label]
-        
-    # def next_unseen_batch(self, batch_size):
-        # idx_unseen = torch.randperm(self.ntest_unseen)[0:batch_size]
-        # batch_unseen_feature = self.data['test_unseen']['resnet_features'][idx_unseen]
-        
-        # idx_unseen =torch.randint(0, self.unseenclasses.shape[0], (batch_size,))
-        # unseen_label=self.unseenclasses[idx_unseen]
-        # batch_unseen_att=self.aux_data[unseen_label]
-        
-        # return [batch_unseen_att, unseen_label]
+        return batch_label, [batch_feature, batch_att]
+
+    def read_pollendata(self):
+        matcontent = self.get_pollen_data()
+
+        feature = matcontent['features']
+        label = matcontent['labels'].astype(int).squeeze()
+        image_files = matcontent['image_files'].squeeze()
+
+        trainval_loc = matcontent['trainval_loc'].squeeze()
+        train_loc = matcontent['train_loc'].squeeze()
+        val_unseen_loc = matcontent['val_loc'].squeeze()
+        test_seen_loc = matcontent['test_seen_loc'].squeeze()
+        test_unseen_loc = matcontent['test_unseen_loc'].squeeze()
+        self.aux_data = torch.from_numpy(matcontent['att'].T).float().to(self.device)
+
+        # TODO check if scaling is needed
+        ########################################################################
+        # preprocessing
+        scaler = MinMaxScaler()
+
+        train_feature = scaler.fit_transform(feature[trainval_loc])
+        test_seen_feature = scaler.transform(feature[test_seen_loc])
+        test_unseen_feature = scaler.transform(feature[test_unseen_loc])
+        ########################################################################
+
+        train_feature = torch.from_numpy(train_feature).float().to(self.device)
+        test_seen_feature = torch.from_numpy(test_seen_feature).float().to(self.device)
+        test_unseen_feature = torch.from_numpy(test_unseen_feature).float().to(self.device)
+
+        train_label = torch.from_numpy(label[trainval_loc]).long().to(self.device)
+        test_unseen_label = torch.from_numpy(label[test_unseen_loc]).long().to(self.device)
+        test_seen_label = torch.from_numpy(label[test_seen_loc]).long().to(self.device)
+
+        self.seenclasses = torch.from_numpy(np.unique(train_label.cpu().numpy())).to(self.device)
+        self.novelclasses = torch.from_numpy(np.unique(test_unseen_label.cpu().numpy())).to(self.device)
+        self.ntrain = train_feature.size()[0]
+        self.ntrain_class = self.seenclasses.size(0)
+        self.ntest_class = self.novelclasses.size(0)
+        self.train_class = self.seenclasses.clone()
+        self.allclasses = torch.arange(0, self.ntrain_class+self.ntest_class).long()
+        self.attri_name = matcontent['attri_name']
+
+        self.train_mapped_label = map_label(train_label, self.seenclasses)
+
+        self.data = {}
+        self.data['train_seen'] = {}
+        self.data['train_seen']['resnet_features'] = train_feature
+        self.data['train_seen']['labels'] = train_label
+        self.data['train_seen'][self.auxiliary_data_source] = self.aux_data[train_label]
+
+        self.data['train_unseen'] = {}
+        self.data['train_unseen']['resnet_features'] = None
+        self.data['train_unseen']['labels'] = None
+
+        self.data['test_seen'] = {}
+        self.data['test_seen']['resnet_features'] = test_seen_feature
+        self.data['test_seen']['labels'] = test_seen_label
+
+        self.data['test_unseen'] = {}
+        self.data['test_unseen']['resnet_features'] = test_unseen_feature
+        self.data['test_unseen'][self.auxiliary_data_source] = self.aux_data[test_unseen_label]
+        self.data['test_unseen']['labels'] = test_unseen_label
+
+        self.novelclass_aux_data = self.aux_data[self.novelclasses]
+        self.seenclass_aux_data = self.aux_data[self.seenclasses]
 
     def read_matdataset(self):
 
@@ -129,7 +199,7 @@ class DATA_LOADER(object):
         test_seen_feature = torch.from_numpy(test_seen_feature).float().to(self.device)
         test_unseen_feature = torch.from_numpy(test_unseen_feature).float().to(self.device)
         self.ntest_unseen = test_unseen_feature.size()[0]
-        
+
         train_label = torch.from_numpy(label[trainval_loc]).long().to(self.device)
         test_unseen_label = torch.from_numpy(label[test_unseen_loc]).long().to(self.device)
         test_seen_label = torch.from_numpy(label[test_seen_loc]).long().to(self.device)
@@ -310,5 +380,4 @@ class DATA_LOADER(object):
             self.data['train_seen_unseen_mixed']['glove'] = torch.cat((self.data['train_seen']['glove'],self.data['train_unseen']['glove']),dim=0)
         if use_hie:
             self.data['train_seen_unseen_mixed']['wordnet'] = torch.cat((self.data['train_seen']['wordnet'],self.data['train_unseen']['wordnet']),dim=0)
-
-#d = DATA_LOADER()
+            
